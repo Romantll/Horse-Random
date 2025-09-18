bc08b83ed6256afcb2ec403f7e67bd7a58408845
Roman Rendon <Roman.Rendon123@gmail.com>
2025-09-18 12:57:12 -0500

    Initial Commit

---
+++ bin/horse_racer.dart
@@ -0,0 +1,5 @@
+import 'package:horse_racer/horse_racer.dart' as horse_racer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${horse_racer.calculate()}!');
+}
---
+++ test/horse_racer_test.dart
@@ -0,0 +1,8 @@
+import 'package:horse_racer/horse_racer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
