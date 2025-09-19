05b1dcdb9da34be1fe77057295baed5ca1d474a7
Roman Rendon <Roman.Rendon123@gmail.com>
2025-09-18 22:30:35 -0500

    added logic to horse.dart

--- lib/horse.dart
+++ lib/horse.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
 class Horse {
@@ -9,6 +11,36 @@ class Horse {
   int acceleration;
   int luck;
   
+  static final adjectivesFile = File('assets/nouns-1.txt');
+  static final nounsFile = File('assets/adj-1.txt');
+
+
+  static Future<List<String>> getNounsList() async
+  {
+      return await nounsFile.readAsLines(encoding: utf8);
+  }
+
+
+  static Future<List<String>> getAdjectivesList() async
+  {
+      return await adjectivesFile.readAsLines(encoding: utf8);
+  }
+
+  //Generating a random name 
+  static String generateName(List<String> nouns, List<String> adjectives)
+  {
+      var rand = Random();
+      var randomNoun = nouns[rand.nextInt(nouns.length)];
+      var randomAdjectives = adjectives[rand.nextInt(adjectives.length)];
+
+      //50% chance it is just an adjective & noun, else just noun
+      if(rand.nextBool())
+      {
+        return "$randomAdjectives $randomNoun";
+      }else {
+        return randomNoun;
+      }
+  }
 
   Horse()
     : currDistance = 0,
@@ -21,5 +53,6 @@ class Horse {
       {
         currStamina = maxStamina;
       }
+  
 }
   
